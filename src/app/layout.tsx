8e404554a0720b39527faaa56dee9bd572f5b749
Julio Eliseo Valls Martinez <julioevm@gmail.com>
2025-12-17 13:00:28 +0000

    Can we use the google font 'Eagle lake' for the headers to give it a mor

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 import type {Metadata} from 'next';
 import './globals.css';
-import { Inter, Space_Grotesk } from 'next/font/google';
+import { Inter, Eagle_Lake } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { SiteHeader } from '@/components/layout/header';
 import { SiteFooter } from '@/components/layout/footer';
 import { Toaster } from "@/components/ui/toaster";
 
 const fontBody = Inter({ subsets: ['latin'], variable: '--font-body' });
-const fontHeadline = Space_Grotesk({ subsets: ['latin'], variable: '--font-headline' });
+const fontHeadline = Eagle_Lake({ subsets: ['latin'], weight: '400', variable: '--font-headline' });
 
 export const metadata: Metadata = {
   title: 'Enroth Archive',
@@ -24,7 +24,7 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&family=Eagle+Lake&display=swap" rel="stylesheet" />
       </head>
       <body className={cn('min-h-screen bg-background font-body antialiased', fontBody.variable, fontHeadline.variable)}>
         <div className="relative flex min-h-screen flex-col">
